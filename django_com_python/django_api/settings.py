bebbc4d9fdfdce53f4a1f5e46b6d950063857479
sara <sara27082011@gmail.com>
2021-12-18 11:52:58 -0300

    add account

--- django_com_python/django_api/settings.py
+++ django_com_python/django_api/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'produto.apps.ProdutoConfig',
-    'cadastro.apps.CadastroConfig'
+    # 'cadastro.apps.CadastroConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- django_com_python/produto/urls.py
+++ django_com_python/produto/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from produto import views
 
 urlpatterns = {
     path('', views.metodo),
